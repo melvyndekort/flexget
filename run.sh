e67524c523b2535555bf361985d377c7a2002a57
Melvyn de Kort <m.de.kort@portbase.com>
2020-09-07 20:58:46 +0200

    Set the timezone with the TZ variable

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+## Remove old lock file
 rm -f /root/.flexget/.config-lock
 
+## Set the timezone if the TZ variable is set
+[[ ! -z "$TZ" ]] && echo $TZ > /etc/timezone
+
+## Start the daemon
 flexget daemon start --autoreload-config
